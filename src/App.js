f3713f238cca796b49ca51f19c15278e4ab9b3d2
fernando peguero <fernandoapeguero@gmail.com>
2020-12-01 13:24:01 -0500

    remove MovieCard Component issues with key prop not been set when returning from component

--- src/App.js
+++ src/App.js
@@ -6,24 +6,24 @@ import React , {useState} from "react";
 
 function App() {
 
-  const [inputName , changeInputName] = useState("")
+  const [inputQuery , setInputQuery] = useState("")
   const [movies , setMovieList] = useState([])
 
 
   const updateQueryInput = (e) => {
     const movieName = e.target.value
-    changeInputName(movieName)
+    setInputQuery(movieName)
   }
 
 
   const movieApiSearch = async (e) => {
     e.preventDefault();
 
-    console.log(`Calling the Movie Api with ${inputName}`)
-    const fixedQueryString = inputName ? inputName.split(" ").join("%20") : "crime";
+    console.log(`Calling the Movie Api with ${inputQuery}`)
+    const fixedQueryString = inputQuery ? inputQuery.split(" ").join("%20") : "crime";
     
-    const apiUrl =  `https://api.themoviedb.org/3/search/movie?api_key=<<api key>>&language=en-US&query=${fixedQueryString}&page=1&include_adult=false`  
-    
+    const apiUrl =  `https://api.themoviedb.org/3/search/movie?api_key=c6730ed582b4839591ed194c26e09262&language=en-US&query=${fixedQueryString}&page=1&include_adult=false`  
+    console.log(apiUrl)
     const result = await fetch(apiUrl)
     
     const json = await result.json()
@@ -38,7 +38,7 @@ function App() {
   return (
     <div className="container">
           <h1 className="title">React Movie Search</h1>
-          <SearchMovies updateQueryInput={updateQueryInput} queryValue={inputName} movieApiSearch={movieApiSearch} />
+          <SearchMovies updateQueryInput={updateQueryInput} queryValue={inputQuery} movieApiSearch={movieApiSearch} />
           {movies.length > 0 && <MovieList  movies={movies}/>}
     </div>
   );
